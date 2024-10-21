3ad328018fd3cc9b412eb5b3f72439e34058e495
EvgenSavenok <eugen.savenok@gmail.com>
2024-10-21 16:58:59 +0300

    Fix: error messages

--- Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51fc44d23c67e6c7aa3025c249071afcd54af15f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+636d3a14218a5a5fad9704d4eabc310488044484")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
