d08f2f3ff685ccc7fe7056dfcd67c5ae2ddabfa4
EvgenSavenok <eugen.savenok@gmail.com>
2024-11-23 21:46:56 +0300

    Fix: swagger

--- Infrastructure/Extensions/ServiceExtensions.cs
+++ Infrastructure/Extensions/ServiceExtensions.cs
@@ -113,34 +113,34 @@ public static class ServiceExtensions
     }
     public static void ConfigureSwagger(this IServiceCollection services)
     {
-        // services.AddSwaggerGen(s =>
-        // {
-        //     s.SwaggerDoc("v1", new OpenApiInfo { Title = "Library API", Version = "v1"
-        //     });
-        // s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-        // {
-        //     In = ParameterLocation.Header,
-        //     Description = "Place to add JWT with Bearer",
-        //     Name = "Authorization",
-        //     Type = SecuritySchemeType.ApiKey,
-        //     Scheme = "Bearer"
-        // });
-        // s.AddSecurityRequirement(new OpenApiSecurityRequirement()
-        // {
-        //     {
-        //         new OpenApiSecurityScheme
-        //         {
-        //             Reference = new OpenApiReference
-        //             {
-        //                 Type = ReferenceType.SecurityScheme,
-        //                 Id = "Bearer"
-        //             },
-        //             Name = "Bearer",
-        //         },
-        //         new List<string>()
-        //     }
-        // });
-        // });
+        services.AddSwaggerGen(s =>
+        {
+            s.SwaggerDoc("v1", new OpenApiInfo { Title = "Library API", Version = "v1"
+            });
+        s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Place to add JWT with Bearer",
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
+        s.AddSecurityRequirement(new OpenApiSecurityRequirement()
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+                    Name = "Bearer",
+                },
+                new List<string>()
+            }
+        });
+        });
     }
     
     public static void ConfigureLoggerService(this IServiceCollection services) =>
